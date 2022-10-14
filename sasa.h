9142277756fb966fb6e34f21f53412aa4884812c
Sasanka <71892768+sasankabandara@users.noreply.github.com>
2022-10-15 01:34:51 +0530

    Create sasa.h

---
+++ sasa.h
@@ -0,0 +1,542 @@
+<!DOCTYPE html>
+<!--
+  Google HTML5 slide template
+
+  Authors: Luke Mahé (code)
+           Marcin Wichary (code and design)
+           
+           Dominic Mazzoni (browser compatibility)
+           Charles Chen (ChromeVox support)
+
+  URL: http://code.google.com/p/html5slides/
+-->
+
+<html>
+<head>
+<title>Introduction to HTML5</title>
+
+<meta charset='utf-8'>
+<script src='slides.js'></script>
+<script type="text/javascript" src="jquery.js"></script>
+<script>
+$(document).ready(function() {
+	$("#today").text(new Date().toDateString());
+})
+</script>
+</head>
+
+<style>
+
+</style>
+
+<body style='display: none'>
+
+<section class='slides layout-regular template-default'>
+
+	<article>
+	<h1>
+	Introduction to HTML5 Stuff
+	</h1>
+	<p>
+	Raymond Camden
+	<br>
+	<span id="today"></span>
+	</p>
+	<img src="images/HTML5_Logo.png" style="position:absolute; right:0px;bottom:20px">
+	</article>
+
+  <article>
+    <h3>
+      The Code
+    </h3>
+    <ul>
+      <li>https://github.com/cfjedimaster/HTML-Code-Demos</li>
+    </ul>
+  </article>     
+ 
+  <article>
+    <h3>
+      Who am I?
+    </h3>
+		<ul>
+			<li>Developer Evangelist for Adobe</li>
+			<li>@cfjedimaster</li>
+			<li>http://www.raymondcamden.com
+			<li>rcamden@adobe.com</li>
+		</ul>
+  </article>     
+
+		<article>
+			<h3>Agenda</h3>
+			<ul>
+				<li>"HTML5" WTF OMG</li>
+				<li>Shiny New Tags</li>
+				<li>Explosive JavaScript APIs</li>
+        <li>Bleeding Edge CSS</li>
+				<li>Awesome Web Sites</li>
+			</ul>
+			<img src="images/robocop%20unicorn.jpg" style="position:absolute;top:0px;right:0px;">
+		</article>
+
+		<article>
+			<h3>HTML5</h3>
+			<ul>
+				<li>What exactly is it?</li>
+				<li>Where can I use it?</li>
+				<li>When can I use it?</li>
+				<li>I still don't know HTML4!</li>
+			</ul>
+		</article>
+
+		<article class="fill">
+			<p>
+				<img src="images/htmldrug.png">
+			</p>
+		</article>
+		
+		<article>
+				<h3>HTML5 - Let's be anal</h3>
+				<ul>
+					<li>Next release of the HTML standard</li>
+					<li><abbr title="World Wide Web Consortium">W3C</abbr> and <abbr title="Web Hypertext Application Technology Working Group">WHATWG</abbr></li>
+					<li>Began June 2004</li>
+					<li>draft status</li>
+					<li>Gory details: http://www.w3.org/TR/html5</li>
+				</ul>
+			<p>
+			<img src="images/geek.jpg">
+			</p>
+		</article>
+
+		<article>
+				<h3>HTML5 - Fraking Unicorns!</h3>
+				<ul>
+					<li>It's HTML and JavaScript and CSS!!</li>
+					<li>It's Mobile!</li>
+					<li>It's Unicorns!</li>
+					<li>It works EVERYWHERE!</li>
+				</ul>
+			<p>
+			<img src="images/Smarmy-Sales-Guy.jpg">
+			</p>
+		</article>
+
+		<article>
+			<div class="build">
+				<p>Stop thinking HTML5</p>
+				<p>Start thinking <b>HTML (with CSS, JavaScript)</b></p>
+				<p>Or even better - Web Standards</p>
+				<p>Have a realistic idea of what works where</p>
+				<p>Congrats - the web is fun again</p>
+				<p><img src="images/man_yelling.jpg"></p>
+			</div>
+		</article>
+
+		<article>
+			<h3>One Important Note</h3>
+			<ul>
+				<li>Add: &lt;!DOCTYPE html&gt;</li>
+				<li>Flags the browser to bring the awesome.</li>
+				<li>IE, especially, will be happy to act like a complete turd without it</li>
+			</ul>
+      <img src="images/Dwight-Schrute-Internet-Explorer.jpg" align="right">
+		</article>
+
+		<article>
+				<h3>Tags</h3>
+				<ul>
+					<li>Form-related</li>
+					<li>Audio/Video</li>
+					<li>Schematic</li>
+					<li>Misc</li>
+				</ul>
+        <img src="images/HTMLkitty.jpg" align="right">
+		</article>
+
+		<article class="fill">
+			<p>
+				<img src="images/oh_no_you_didnt.gif">
+			</p>
+		</article>
+
+		<article>
+			<h3>Forms</h3>
+			<ul>
+				<li>New Input Types</li>
+				<li>Validation</li>
+				<li>Placeholder</li>
+				<li>Autofocus</li>
+				<li>Datalists (autocomplete)</li>
+				<li>Multiple file upload</li>
+				<li>Form overrides for fields</li>
+				<li>The Output tag</li>
+			</ul>
+		</article>
+
+		<article>
+			<h3>Input Types</h3>
+			<ul>
+				<li>New Input Types (13!)</li>
+				<li>All fail nicely (especially in IE)</li>
+				<li>Opera seems the most advanced here</li>
+				<li><a href="code/forms/1_inputtype.html" target="_new">Run It</a></li>
+			</ul>
+		</article>
+
+		<article>
+			<h3>Validation</h3>
+			<ul>
+				<li>&lt;input type="text" name="foo" required&gt;</li>
+				<li>Multiple types of validation</li>
+				<li>Some cases validates the type</li>
+				<li>Error messages customizable/styleable</li>
+				<li>Can disable: &lt;form novalidate&gt;</li>
+				<li><a href="code/forms/2_validation.html" target="_new">Run It</a></li>
+			</ul>
+		</article>
+
+	<article class="fill">
+		<p>
+			<img src="images/thx-captain-obvious.jpg">
+		</p>
+	</article>
+
+	 <article>
+       <h3>Placeholder and Autofocus</h3>
+       <ul>
+           <li>&lt;input type="text" name="foo" placeholder="Foo"&gt;</li>
+           <li>&lt;input type="text" name="foo" autofocus&gt;</li>
+           <li>Don't do em both</li>
+           <li><a href="code/forms/3_placeholder.html" target="_new">Run Placeholder</a></li>
+           <li><a href="code/forms/4_autofocus.html" target="_new">Run Autofocus</a></li>
+ 		</ul>
+	</article>
+
+        <article>
+            <h3>Datalists</h3>
+            <ul>
+				<li>Autocomplete</li>
+    			<li><a href="code/forms/5_datalist.html" target="_new">Run Datalist</a>
+      		</ul>
+			<p>
+			<code>
+				&lt;input type="text" name="foo" list="beer"&gt;<br/>
+				&lt;datalist id="beer"&gt;<br/>
+				&lt;option value="LA 31"&gt;<br/>
+				&lt;option value="Sam Adams"&gt;<br/>
+				&lt;option value="Stone IPA"&gt;<br/>
+				&lt;option value="Abita"&gt;<br/>
+				&lt;option value="Miller"&gt;<br/>
+				&lt;/datalist&gt;<br/>
+			</code>
+			</p>
+  		</article>
+
+	<article>
+      <h3>Multiple file upload</h3>
+      <ul>
+          <li>&lt;input type="file" name="foo" multiple&gt;</li>
+          <li>Also applies to other field types</li>
+          <li><a href="code/forms/6_formmultiple.html" target="_new">Run Multiple</a></li>
+		</ul>
+	</article>
+
+	<article>
+      <h3>Form overrides</h3>
+      <ul>
+          <li>Fields can override form, action, enctype, method, target</li>
+          <li>A field outside a form can be included in the form (um, why?)</li>
+		  <li>A submit button can bypass the default action (um, why?)</li>
+          <li><a href="code/forms/7_formoverride1.html" target="_new">Run Override</a></li>
+		</ul>
+	</article>
+
+	<article>
+      <h3>The Output Tag</h3>
+      <ul>
+		  <li>Used for the result of a calculation</li>
+		  <li>Can note what form fields it relates to</li>
+          <li><a href="code/forms/9_output1.html" target="_new">Run Output</a></li>
+		</ul>
+		<img src="images/kirk-yelling-kahn.jpg" align="right">
+	</article>
+
+
+        <article>
+            <h3>Form Support</h3>
+            <iframe src="http://caniuse.com/forms/embed/"></iframe>
+        </article>
+
+        <article>
+            <h3>Video</h3>
+            <ul>
+                <li>&lt;video&gt;</li>
+                <li>Autoplay (please don't), controls, looping, preload</li>
+                <li>No Flash!</li>
+                <li>That's it!!</li>
+            </ul>
+        </article>
+
+        <article>
+            <h3>Oh Snap!</h3>
+                <img src="images/wtf.jpg">
+        </article>
+
+      <article>
+          <h3>Video</h3>
+          <ul>
+              <li>Codecs and Containers (think of zip files)</li>
+              <li>Containers: Ogg, mpeg4, and WebM</li>
+			  <li>Codocs: H.264, Theora, VP8</li>
+			  <li>Don't forget audio...</li>
+              <li>No DRM</li>
+              <li><a href="code/video/" target="_new">Run It</a></li>
+          </ul>
+      </article>
+
+
+        <article>
+            <h3>Video Support</h3>
+            <iframe src="http://caniuse.com/video/embed/"></iframe>
+        </article>
+
+      <article>
+          <h3>Audio</h3>
+          <ul>
+              <li>&lt;audio&gt;</li>
+              <li>Autoplay (please don't), controls, looping, preload</li>
+              <li>No Flash!</li>
+              <li>That's it!! (you know what's coming up next...)</li>
+          </ul>
+      </article>
+
+      <article class="fill">
+          <h3>Think of this</h3>
+          <img src="images/baby-seal.jpg">
+      </article>
+
+      <article>
+          <h3>Audio</h3>
+          <ul>
+              <li>Ogg, MP3 (insert evil license warning here), WAV, AAC</li>
+              <li>No DRM</li>
+              <li>JavaScript API which includes a 'canPlayType' - <a href="http://html5doctor.com/html5-audio-the-state-of-play/">Reference</a>, returns "probably", "maybe", or "" (no, seriously)</li>
+              <li>(By the way, there is "support" for pure audio created in the browser.)</li>
+              <li><a href="code/audio/" target="_new">Run It</a>
+          </ul>
+      </article>
+
+      <article>
+          <h3>Audio Support</h3>
+          <iframe src="http://caniuse.com/audio/embed/"></iframe>
+      </article>
+
+      <article>
+          <h3>Schematic</h3>
+          <ul>
+              <li>Describes the content, not the layout</li>
+              <!--
+              <li>Example: &lt;em&gt; versus &lt;cite&gt;</li>
+            -->
+              <li>Meant to prent DIV-itis</li>
+              <li>Some of them: article, aside, details, summary, figure, figcaption, footer, header, hgroup, mark, meter, nav, progress, ruby, rt, rp, section, time, wbr</li>
+              <li><a href="code/schematic" target="_new">More Awesome Code Samples</a>
+          </ul>
+      </article>
+
+      <article>
+          <h3>Schematic Support</h3>
+          <iframe src="http://caniuse.com/html5semantic/embed"></iframe>
+      </article>
+
+      <article>
+          <h3>Data Attributes</h3>
+          <ul>
+              <li>Allows for <i>any</i> attribute in your HTML</li>
+              <li>Prefix your attribute with data-</li>
+              <li>Available via CSS/JavaScript</li>
+              <li>Example: &lt;b data-ray="theawesome" data-clothing="he wears the best"&gt;</li>
+              <li><a href="code/data/" target="_new">Even More Awesome Code Samples</a></li>
+          </ul>
+      </article>
+
+      <article>
+          <h3>Data Attribute Support</h3>
+          <iframe src="http://caniuse.com/dataset/embed/"></iframe>
+      </article>
+
+      <article>
+        <!--<h3>JavaScript APIs!</h3>-->
+        <p>
+        <img src="images/js.png" style="height:100%;" class="centered">
+        </p>
+      </article>
+
+      <article>
+          <h3>Geolocation</h3>
+          <ul>
+              <li>Not just for mobile</li>
+              <li>Requires user approval (and they can change their minds!)</li>
+              <li>Supports options for accuracy, caching, and timeout</li>
+              <li>Support for firing automatically on position change</li>
+              <li>Can fail</li>
+              <li><a href="code/geo" target="_new">Super Cool Code Samples</a></li>
+              <li><a href="http://www.raymondcamden.com/index.cfm/2011/11/14/How-to-give-dynamic-driving-directions-to-your-customers">Blog Demo</a></li>
+          </ul>
+      </article>
+
+      <article>
+          <h3>Geolocation Support</h3>
+          <iframe src="http://caniuse.com/geolocation/embed/"></iframe>
+      </article>
+
+      <article>
+          <h3>Local(Session)Storage</h3>
+          <ul>
+              <li>Like cookies, but not</li>
+              <li>5 megs (who could ever need more???)</li>
+              <li>Key/Value pairs, simple values (but JSON works great)</li>
+              <li>API via getItem, setItem, removeItem</li>
+              <li>File based</li>
+              <li><a href="code/storage" target="_new">Super Cool Code Samples</a></li>
+          </ul>
+      </article>
+
+      <article>
+          <h3>LocalStorage Support</h3>
+          <iframe src="http://caniuse.com/namevalue-storage/embed/"></iframe>
+      </article>
+
+      <article>
+          <h3>IndexedDB Storage</h3>
+          <ul>
+              <li>NoSQL/Object based persistance</li>
+              <li>Much deeper (and therefore more complex) than localStorage.</li>
+              <li>Stop using WebSQL - this is the future (whether you like it or not).</li>
+              <li><a href="code/indexdb" target="_new">Super Cool Code Samples</a></li>
+          </ul>
+      </article>
+
+      <article>
+          <h3>IndexedDB Support</h3>
+          <iframe src="http://caniuse.com/indexeddb/embed/"></iframe>
+      </article>
+
+      <article>
+          <h3>WebSQL Support (in case you care...)</h3>
+          <iframe src="http://caniuse.com/sql-storage/embed/"></iframe>
+      </article>
+
+      <style>
+      #testCanvas {
+        position:absolute;
+        top:0px;
+        right:0px;
+      }
+      </style>
+      <script>
+      function doCanvas() {
+        var item = document.getElementById('testCanvas');
+        var ctx = item.getContext('2d');
+        
+        //clear 
+        item.width = item.width;
+
+        ctx.beginPath();
+        ctx.moveTo(50, 50);
+        ctx.strokeStyle = "#00ff00";
+
+        for (var i = 1; i < 50; i++) {
+          var x = Math.random()*250;
+          var y = Math.random()*250;
+          ctx.lineTo(x, y);
+          ctx.stroke();
+          ctx.moveTo(x, y);
+        }
+              
+      }
+      </script>
+      <article onslideenter="doCanvas()">
+          <h3>Canvas API</h3>
+          <ul>
+              <li>A tag which acts like, well, a canvas</li>
+              <li>Very rich <b>drawing</b> API</li>
+              <li>Absolutely no <b>animation</b> API</li>
+              <li>Good libraries out there though that mimic it (easeljs.com)</li>
+              <li><a href="code/canvas" target="_new">Pretty Code Samples</a>
+          </ul>
+          <img src="images/freakycat.png" style="position:absolute;right:20px;bottom:20px; height:350px">
+          <canvas id="testCanvas" width="250" height="250">Your browser is weak</canvas>
+      </article>
+
+      <article>
+          <h3>Canvas Support</h3>
+          <iframe src="http://caniuse.com/canvas/embed/"></iframe>
+      </article>
+
+      <article>
+          <h3>Offline Support</h3>
+          <ul>
+              <li>A way to tell the browser what to store for offline use</li>
+              <li>ini file format</li>
+              <li>IMO, delicate as fine china in an earthquake</li>
+              <li><a href="code/offline" target="_new">Pretty Code Samples</a></li>
+          </ul>
+          <img src="images/offline.jpg" style="position:absolute;right:20px;bottom:20px; height:350px">
+      </article>
+
+      <article>
+          <h3>Offline Support</h3>
+          <iframe src="http://caniuse.com/offline-apps/embed/"></iframe>
+      </article>
+
+      <article>
+          <h3>Bleeding Edge CSS</h3>
+          <ul>
+            <li>CSS Regions (<a href="http://dev.w3.org/csswg/css3-regions/">w3c spec</a>, <a href="http://labs.adobe.com/technologies/cssregions/">Adobe Labs</a>)</li>
+            <li>CSS Exclusions (<a href="http://dev.w3.org/csswg/css3-exclusions/">w3c spec</a>)</li>
+            <li>CSS Shaders (<a href="http://dvcs.w3.org/hg/FXTF/raw-file/tip/custom/index.html">w3c spec</a>)</li>
+            <li>Oh yeah - all of the above is being pioneered by Adobe (and others)</li>
+            <li>More info - <a href="http://html.adobe.com/webstandards/">Web Standards</a></li>
+          </ul>
+
+      </article>
+
+      <article>
+          <h3>Sites</h3>
+          <ul>
+              <li>HTML5 Rocks (html5rocks.com)</li>
+              <li>WhichElement? (whichelement.com)</li>
+              <li>Dive Into HTML5 (diveintohtml5.info)</li>
+              <li>HTML5 Doctor (html5doctor.com)</li>
+              <li>When can I use... (canuiuse.com)</li>
+              <li>Adobe Dev Center - HTML5 and CSS3 (www.adobe.com/devnet/html5.html)</li>
+      		  <li>HTML5 Weekly (html5weekly.com)</li>
+              <li>HTML5 Bookmarks (html5bookmarks.com)</li>
+          </ul>
+      </article>
+          
+      <article>
+          <h3>Credit</h3>
+          <ul>
+              <li>This slide deck built on <a href="http://code.google.com/p/html5slides/">html5slides</a></li>
+          </ul>
+      </article>
+
+      <!--
+      To consider:
+      document.querySelector/document.querySelectorAll (#queryselector)
+      Drag/Drop
+      xmlhttprequest2
+      file/filereader
+      bloburls
+      blobbuilder
+
+
+http://html5doctor.com/css3-pseudo-classes-and-html5-forms/
+      -->
+
+    </section>
+
+  </body>
+</html>
